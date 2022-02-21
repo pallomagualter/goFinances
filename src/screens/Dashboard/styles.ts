d03352fba457abedfff26394c38df4ecfb204229
pallomagualter <pallomagualter@gmail.com>
2022-02-21 11:45:00 -0300

    using themes with styled-components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center; 
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: purple;
+  color: ${({ theme }) => theme.colors.title}
 `;
