44f27f89e4f137b7b3a85d8fec72f3b94a28ef93
pallomagualter <pallomagualter@gmail.com>
2022-02-21 12:21:47 -0300

    using external fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
 
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
+  const [fonstsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  //para segurar a tela de splash enquanto carrega as fontes
+  if (!fonstsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard title='Dashboard' />
