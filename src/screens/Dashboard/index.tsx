4611928d3de1ecb47e0e35b8c31bcacaae361dde
pallomagualter <pallomagualter@gmail.com>
2022-02-21 17:19:28 -0300

    usando proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 import {
   Container,
-  Header
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
 } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
       <Header>
-
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/33058536?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Palloma</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
       </Header>
     </Container>
   );
